5f4214c86a740a52d74dd76e08f648658580c2d8
Your Name <you@example.com>
2020-07-20 16:08:15 +0900

    navbar

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ from flask import Flask ,render_template
 app = Flask(__name__)
 app.debug=True
 
-@app.route('/data')
+@app.route('/')
 def index():
     print("Success")
     # return "TEST"
-    return render_template('home.html')
+    return render_template('home.html',hello="jhch")
+
+@app.route('/about')
+def about():
+    print("Success")
+    # return "TEST"
+    return render_template('about.html',hello="jhch")
+
+@app.route('/articles')
+def articles():
+    print("Success")
+    # return "TEST"
+    return render_template('articles.html',hello="jhch")
 
 if __name__ == "__main__":
     # app.run(host = '0.0.0.0',port='8080')
